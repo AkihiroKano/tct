df63ad365f2f4581f84294825ec9990f2616ba56
Vladimir Orlov <144994045+AkihiroKano@users.noreply.github.com>
2025-10-08 17:03:27 +0300

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# 1. Настройка физического интерфейса (ens33)
+echo "1. Настройка физического интерфейса ens33:"
+# nmcli — утилита NetworkManager для управления сетевыми соединениями. con = connection работает с соединениями
+# ethernet - проводной интерфейс. ifname — имя нового интерфейса. con-name — имя соединения в NetworkManager.
+sudo nmcli con add type ethernet ifname ens33 con-name static-ens33 
+sudo nmcli con modify static-ens33 ipv4.addresses 10.100.0.2/24
+sudo nmcli con modify static-ens33 ipv4.gateway 10.100.0.1
+sudo nmcli con modify static-ens33 ipv4.dns 8.8.8.8
+# ipv4.method — метод получения IP. manual — статическая настройка (не DHCP)
+sudo nmcli con modify static-ens33 ipv4.method manual
+sudo nmcli con up static-ens33
+
+# 2. Создание виртуального интерфейса br0
+echo "2. Создание виртуального интерфейса br0:"
+# bridge — тип интерфейса мост, позволяет объединять несколько интерфейсов
+sudo nmcli con add type bridge ifname br0 con-name br0
+sudo nmcli con modify br0 ipv4.addresses 10.100.0.3/24
+sudo nmcli con modify br0 ipv4.method manual
+sudo nmcli con up br0
+
+# 3. Проверка связи между интерфейсами
+echo "3. Проверка связи между интерфейсами:"
+# -c 4 отправка 4 пакетов. -I  указывает через какой интерфейс отправлять пакеты
+ping -c 4 -I ens33 10.100.0.3 # реальный интерфейс
+ping -c 4 -I br0 10.100.0.2 # виртуальный интерфейс
+
+# 4. Определение MAC-адреса виртуального интерфейса
+echo "4. Определение MAC-адреса виртуального интерфейса br0"
+echo "MAC-адрес:"
+ip link show br0 | grep link/ether
